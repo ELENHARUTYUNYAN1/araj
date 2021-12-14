632c270ffe39634c66ed179044bf99e9732685c2
User <you@example.com>
2021-12-14 06:10:29 -0800

    Added update post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,6 +15,11 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
+	def update
+		@post = Post.find(params[:id])
+		@post.update(post_params)
+		redirect_to post_path(@post)
+	end
 	private
 	def post_params
 		params.require(:post).permit(:title, :body)
